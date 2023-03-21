9877acf4d831239672c267e6e7b41abb8da91035
Nikita <nikitkacool29@gmail.com>
2023-03-21 17:31:08 +0300

    final version

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,38 +1,42 @@
-import md5 from "blueimp-md5";
 
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=963c7992ad4d3d9ba2b1f5df589782ae';
+    _baseOffset = 210;
+
 
     getResource = async (url) => {
         let res = await fetch(url);
+    
         if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
+    
         return await res.json();
     }
 
-    getHash = (timeStamp) => {
-        return md5( timeStamp + "963c7992ad4d3d9ba2b1f5df589782ae" + "c19d215a353e1e39fc2970c6a9bb506deb30ddbb");
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter)
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(
+            `${this._apiBase}characters/${id}?` + `&${this._apiKey}`
+        );
+        return this._transformCharacter(res.data.results[0])
     }
 
-    getCharacter = (id) => {
-        const timeStamp = + new Date();
-        const hash = this.getHash(timeStamp);
-
-        return this.getResource(
-            `${this._apiBase}${id}?`+
-            "&ts=" + timeStamp +
-            `&${this._apiKey}`+
-            "&hash=" + hash
-            // `${this._apiBase}characters/${id}?${this._apiKey}`
-            // `characters/${id}?${this._apiKey}&ts=${timeStamp}&hash=${hash}`
-            // "https://gateway.marvel.com:443/v1/public/characters/201?apikey=963c7992ad4d3d9ba2b1f5df589782ae"
-            );
+    _transformCharacter = (char) => {
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+            comics: char.comics.items
+        }
     }
 }
 
